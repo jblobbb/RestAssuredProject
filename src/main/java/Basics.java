b33917986b07d6d9bcda1162e89fee7184b97926
Johnny <johnnyhunto@googlemail.com>
2023-01-20 13:59:47 +0000

    added json package and file

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -2,11 +2,15 @@ import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import org.testng.Assert;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
 public class Basics {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         RestAssured.baseURI = "https://rahulshettyacademy.com";
         RestAssured.useRelaxedHTTPSValidation();
@@ -16,8 +20,10 @@ public class Basics {
         //given - all input details
         //when - submit api - resource, http method
         //then - validate the response
+        //content of the file to string -> content of file can convert into byte -> convert byte data to string
 
-        String response = given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json").body(Payload.addPlace())
+        String response = given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
+                .body(new String(Files.readAllBytes(Paths.get("src/main/java/jsonfiles/addPlace.json"))))
                 .when().post("/maps/api/place/add/json")
                 .then().assertThat().statusCode(200).body("scope", equalTo("APP")).header("Server", "Apache/2.4.41 (Ubuntu)").extract().response().asString();
 
