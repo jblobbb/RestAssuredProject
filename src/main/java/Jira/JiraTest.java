735eadd33f4867b097b4d16979313eaac116f6fc
Johnny <johnnyhunto@googlemail.com>
2023-02-03 16:20:18 +0000

    removed part of header

--- src/main/java/Jira/JiraTest.java
+++ src/main/java/Jira/JiraTest.java
@@ -16,14 +16,14 @@ public class JiraTest {
 
         //add comment request
         given().pathParam("key", "10100").log().all()
-                .header("Authorization","Bearer OTk4MTIwNTM0MTU0OjDu6kwD2I0n1bicjTgM1JQuuE/B").header("Cookie","JSESSIONID=483662EABA8E4AFFD28F67487A722894; atlassian.xsrf.token=BKAL-FQAZ-5VOF-4BN3_11e4e84aba3fc4db6906a8da3e2514b930d6ee91_lin").header("Content-Type", "application/json")
+                .header("Authorization","Bearer OTk4MTIwNTM0MTU0OjDu6kwD2I0n1bicjTgM1JQuuE/B").header("Content-Type", "application/json")
                 .body(Files.readAllBytes(Paths.get("src/main/resources/mocks/addComment.json")))
                 .when().post("/rest/api/2/issue/{key}/comment")
                 .then().log().all().assertThat().statusCode(201);
 
         //add attachment
         given().header("X-Atlassian-Token", "no-check")
-                .header("Authorization","Bearer OTk4MTIwNTM0MTU0OjDu6kwD2I0n1bicjTgM1JQuuE/B").header("Cookie","JSESSIONID=483662EABA8E4AFFD28F67487A722894; atlassian.xsrf.token=BKAL-FQAZ-5VOF-4BN3_11e4e84aba3fc4db6906a8da3e2514b930d6ee91_lin").header("Content-Type", "multipart/form-data")
+                .header("Authorization","Bearer OTk4MTIwNTM0MTU0OjDu6kwD2I0n1bicjTgM1JQuuE/B").header("Content-Type", "multipart/form-data")
                 .pathParam("key", "10100").multiPart("file", new File("src/main/resources/Files/jira.txt"))
                 .when().post("/rest/api/2/issue/{key}/attachments")
                 .then().log().all().assertThat().statusCode(200);
